af22c1a42a1c2389dc3290914e806b8705ad37df
pedrojjsbr <80608215+pedrojjsbr@users.noreply.github.com>
2022-10-02 19:06:29 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda online</title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+<body>
+    <header>
+
+    </header>
+    <section>
+        <p id="tlogin"><b>Login</b></p>
+        <form action="login"></form>
+            <label for="fname">Usuário:</label><br>
+            <input id="editinputvar" type="text" id="fname" name="fname" value="John"><br>
+            <label for="lname">Senha:</label><br>
+            <input id="editinputvar" type="text" id="lname" name="lname" value="Doe"><br><br>
+            <input class="editinputsubmit" type="submit" value= "entrar">
+            
+        </form>
+    </section>
+    
+</body>
+</html>
